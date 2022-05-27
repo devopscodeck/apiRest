7e49b569bb4c5f3257b8acd36379a9b2917d2c4d
devopscodeck <you@example.com>
2022-05-27 20:12:47 -0300

    MVC&JsonEncond

--- main.go
+++ main.go
@@ -3,10 +3,16 @@ package main
 import (
 	"fmt"
 
+	"github.com/devopscodeck/apiRest/models"
 	"github.com/devopscodeck/apiRest/routes"
 )
 
 func main() {
+	models.Personalidades = []models.Personalidade{
+		{Nome: "Nome1", Historia: "Historia1"},
+		{Nome: "Nome2", Historia: "Historia2"},
+	}
+
 	fmt.Println("Iniciando o servidor Rest com GO")
 	routes.HandleRequest()
 }
