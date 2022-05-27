f07cd0891b97052b8052e59bb6bacc158f2ce61a
devopscodeck <you@example.com>
2022-05-27 20:02:53 -0300

    modularizando

--- main.go
+++ main.go
@@ -2,20 +2,11 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"net/http"
+
+	"github.com/devopscodeck/apiRest/routes"
 )
-
-func Home(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Home Page")
-}
-
-func HandleRequest() {
-	http.HandleFunc("/", Home)
-	log.Fatal(http.ListenAndServe(":8000", nil))
-}
 
 func main() {
 	fmt.Println("Iniciando o servidor Rest com GO")
-	HandleRequest()
+	routes.HandleRequest()
 }
