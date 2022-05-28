e68ba5fbacc711500292ea4a0312077399b7a380
devopscodeck <you@example.com>
2022-05-27 22:23:42 -0300

    create middleware for content-type in functions

--- routes/routes.go
+++ routes/routes.go
@@ -5,11 +5,13 @@ import (
 	"net/http"
 
 	"github.com/devopscodeck/apiRest/controllers"
+	"github.com/devopscodeck/apiRest/middleware"
 	"github.com/gorilla/mux"
 )
 
 func HandleRequest() {
 	r := mux.NewRouter()
+	r.Use(middleware.ContentTypeMiddleware)
 	r.HandleFunc("/", controllers.Home)
 	r.HandleFunc("/api/personalidades", controllers.TodasPersonalidades).Methods("Get")
 	r.HandleFunc("/api/personalidades/{id}", controllers.RetornaUmaPersonalidade).Methods("Get")
